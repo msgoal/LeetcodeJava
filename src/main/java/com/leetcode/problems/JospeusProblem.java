c28a474a5d2d3e61b01884b6895fada2e60210de
student-at-926243062264 <A@a.com>
2023-11-06 17:40:50 +0530

    Wiremock expirement

--- src/main/java/com/leetcode/problems/JospeusProblem.java
+++ src/main/java/com/leetcode/problems/JospeusProblem.java
@@ -12,17 +12,21 @@ public class JospeusProblem {
         while(i <= n){
             res = (res + k)%i;
             i++;
-            System.out.println(res);
         }
 
         return res+1;
+
+        /*if(n == 1)
+            return 1;
+
+        return (joesphus(n-1, k) + k - 1) % n + 1;*/
     }
     private static int josephus(int n, int k) {
         return n > 1 ? (josephus(n-1, k) + k - 1) % n + 1 : 1;
     }
     public static void main(String[] args) {
 
-        //for(int i=1;i<=20;i++)
-        System.out.println(20+"\t\t"+3+"\t\t"+joesphus(16,3));
+        for(int i=1;i<=20;i++)
+        System.out.println(joesphus(i,3));
     }
 }
